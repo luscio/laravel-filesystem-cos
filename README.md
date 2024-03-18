2a0dbf7c3bad93cde0457af7b2ea5a1bd04ba5d9
luscio <luscio3366@gmail.com>
2024-03-18 11:24:51 +0800

    初始版本

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # laravel-filesystem-cos
-laravel-filesystem-cos
---
+++ src/CosStorageServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Luscio\LaravelFilesystem\Cos;
+
+use Illuminate\Filesystem\FilesystemAdapter;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\ServiceProvider;
+use League\Flysystem\Filesystem;
+use Overtrue\Flysystem\Cos\CosAdapter;
+
+class CosStorageServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot()
+    {
+        Storage::extend('cos', function ($app, $config) {
+            $adapter = new CosAdapter($config);
+            return new FilesystemAdapter(new Filesystem($adapter), $adapter);
+        });
+    }
+
+    /**
+     * Register any application services.
+     */
+    public function register()
+    {
+    }
+}
